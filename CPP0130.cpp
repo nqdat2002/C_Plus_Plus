154acfc59ec0bda5b20f92fb7f8157d2949d2b04
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2021-11-08 09:52:47 +0700

    Update CPP0130.cpp

--- CPP0130.cpp
+++ CPP0130.cpp
@@ -1,25 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 int main(){
-	ios::sync_with_stdio(0); 
-    cin.tie(0);              
-    cout.tie(0);
-	int t; 
-	cin >> t;
+	int t; cin >> t;
 	while (t--) {
-		long long int n; 
-		cin >> n;
+		ll n; cin >> n;
 		while (n % 2 == 0) {
 			cout << 2 << " "; 
 			n /= 2;
 		}
-		long long int i = 3;
+		ll i = 3;
 		while (i <= sqrt(n)) {
 			while (n % i == 0) {
 				cout << i << " ";
 				n /= i;
 			}
-			i+=2;
+			i += 2;
 		}
 		if(n > 2)
 			cout << n;
