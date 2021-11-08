81254192f3045b8b2a839adeb431558cc0c8c794
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2021-11-08 09:42:16 +0700

    Update CPP0109.cpp

--- CPP0109.cpp
+++ CPP0109.cpp
@@ -1,28 +1,27 @@
 #include <bits/stdc++.h>
-#include <algorithm>
-#include <cmath>
 using namespace std;
-int Parity(long long int x) {
-    int c=0,d=0;
+typedef long long ll;
+int Parity(ll x) {
+    int c = 0, d = 0;
     while(x) {
-        int i=x%10;
-        if(i%2==0) c++;
-        else d++;
-        x/=10;
+        int i = x % 10;
+        (i % 2 == 0) ? c ++ : d ++;
+        x /= 10;
     }
-    if(c==d)
+    if(c == d)
     	return true;
     return false;
 }
 int main() {
-   	long long int m,j,i=0;
+   	ll m, j;
+    int cnt = 0;
     cin >> m;
-    if(m%2==0){
-    	for(j = pow(10,m-1); j < pow(10,m); j++){
+    if(m % 2 == 0){
+    	for(j = pow(10, m - 1); j < pow(10, m); j ++){
       	  	if(Parity(j)){
         		cout << j << " ";
-      			i++;
-     			if(i%10==0) cout << endl;
+      			cnt ++;
+     			if(cnt % 10 == 0) cout << endl;
  			}		
     	}
     }
