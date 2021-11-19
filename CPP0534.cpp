cf2b4dab43595fdcbb1471758f01331fa0bd0a7a
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2021-11-19 22:33:27 +0700

    Update CPP0534.cpp

--- CPP0534.cpp
+++ CPP0534.cpp
@@ -1,48 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct vb {
-	string str;
-	int count;
-};
-int tn(string str) {
-	if (str.length() == 1)
-		return 0;
-	for (int i = 0; i < str.length(); i++)
-		if (str[i] != str[str.length() - i - 1])
-			return 0;
-	return 1;
+bool tn(string str) {
+	int n = str.size();
+	if (n == 1)
+		return false;
+	for (int i = 0; i < n; i++)
+		if (str[i] != str[n - i - 1])
+			return false;
+	return true;
 }
-bool Comparation(vb X, vb Y) {
-	if (X.str.length() > Y.str.length())
+bool cmp(pair<string, int> a, pair<string, int> b){
+	if(a.first.length() > b.first.length())
 		return true;
-	if (X.str.length() == Y.str.length() && X.str > Y.str)
+	if(a.first.length() == b.first.length() && a.first > b.first)
 		return true;
 	return false;
 }
-int main() {
-	vb A[10000];
-	char X[1000];
-	int k = 0;
-	while (scanf("%s", &X) != EOF) {
-		for (int j = 0; j < strlen(X); j ++)
-			A[k].str.push_back(X[j]);
-		k ++;
+int main(){
+	vector<pair<string, int> > pr;
+	string s;
+	while(cin >> s){
+		pair<string, int> x;
+		x = make_pair(s, 0);
+		pr.push_back(x);
 	}
-	for (int i = 0; i < k; i ++) {
-		for (int j = 0; j < k; j ++) {
-			if (A[i].str == A[j].str)
-				A[i].count++;
-		}
+	int n = pr.size();
+	for(int i = 0; i < n; i ++){
+		for(int j = 0; j < n; j ++)
+			if(pr[i].first == pr[j].first)
+				pr[i].second ++; 
 	}
-	sort(A, A + k, Comparation);
-	for (int i = 0; i < k; i ++) {
+	sort(pr.begin(), pr.end(), cmp);
+	for (int i = 0; i < n; i ++) {
 		int x = 0;
 		for (int j = 0; j < i; j++)
-			if (A[i].str == A[j].str)
+			if (pr[i].first == pr[j].first)
 				x++;
 		if (x > 0)
 			continue;
-		if (tn(A[i].str))
-			cout << A[i].str << " " << A[i].count << endl;
+		if (tn(pr[i].first))
+			cout << pr[i].first << " " << pr[i].second << endl;
 	}
+	return 0;
 }
