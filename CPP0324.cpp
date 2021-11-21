c797b17faae57370777a454ecf2150f6bb972d2a
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2021-11-21 10:13:35 +0700

    Update CPP0324.cpp

--- CPP0324.cpp
+++ CPP0324.cpp
@@ -1,42 +1,45 @@
-#include<bits/stdc++.h>
-long long n,k,p;
+#include <bits/stdc++.h>
 using namespace std;
-long long Mul(long long a, long long b, long long p){
+#define xau int t; cin >> t; cin.ignore();
+#define so int t; cin >> t;
+typedef long long ll;
+ll n, k, p;
+ll Multi(ll a, ll b, ll p){
 	if(b == 0) 
 		return 0ll;
-	long long tmp = Mul(a, b/2, p);
+	ll tmp = Multi(a, b/2, p);
 	if(b & 1) 
 		return (tmp * 2 + a) % p;
 	return tmp * 2 % p;
 }
-long long PoW(long long n, long long k, long long p){
+ll POW(ll n, ll k, ll p){
 	if(k == 0) 
 		return 1;
 	if(k == 1) 
 		return n % p;
-	long long tmp = PoW(n, k/2, p);
-	long long a = Mul(tmp, tmp, p);
+	ll tmp = POW(n, k / 2, p);
+	ll a = Multi(tmp, tmp, p);
 	if(k & 1) 
 		return a * n % p;
 	return a % p;
 } 
-int main(){ 
-	int t;
-	cin >> t; 
-	cin.ignore();
-	while(t--) { 
-		string str; 
-		long long Mod, b, res = 0; 
-		cin >> str >> b >> Mod; 
-		vector<int> arr(1005); 
-		int len = str.size(); 
-		for(int i = 0 ; i < len; i ++){
-			arr[i] = str[i] - '0';
-		} 
-		for(int i = 0 ; i < len ; i ++){ 
-			res = (res * 10 + arr[i]) % Mod; 
-		}
-		cout << PoW(res, b, Mod) << endl; 
-	} 
+void Solve(){
+	string str; 
+	ll Mod, b, res = 0; 
+	cin >> str >> b >> Mod; 
+	vector<int> arr(1005); 
+	int len = str.size(); 
+	for(int i = 0 ; i < len; i ++)
+		arr[i] = str[i] - '0';
+	for(int i = 0 ; i < len ; i ++)
+		res = ( res * 10 + arr[i] ) % Mod; 
+	cout << POW(res, b, Mod) << endl;
+}
+int main(){
+	int t; cin >> t; cin.ignore();
+	while(t--){
+	   Solve();
+	}
 	return 0;
 }
+
