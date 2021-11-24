2bdf0f48528e2981833288d82f2361d5680f93ad
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2021-11-24 12:31:25 +0700

    Update CPP0747.cpp

--- CPP0747.cpp
+++ CPP0747.cpp
@@ -1,27 +1,5 @@
 #include <iostream>
 #include <string>
-using namespace std;
-int main() {
-	int t;
-	cin >> t;
-	while (t--) {
-		string str;
-	    int zero = 0, ans = 0;
-	    cin >> str;
-	    for (int i = str.size() - 1; i >= 0; i--) {
-		    zero += (str[i] == '0' ? 1 : 0);
-		    if (str[i] == '1') {
-		        if (zero > 1) {
-		        	ans += 3;
-		        	zero -= 2;
-		        }
-		    }
-	    }
-	    cout << ans << endl;
-	}
-	return 0;
-}
-
 void delete_str(std:: string& str,const std::string& deletee ) {
     size_t pos;
     size_t len = deletee.length();
@@ -36,14 +14,15 @@ void delete_str(std:: string& str,const std::string& deletee ) {
 int main() {
 	int t;
 	std::cin >> t;
+	getchar();
 	while(t--){
 		std::string str;
 		getline(std::cin,str);
 		int x = str.length();
 		delete_str(str, "100");
-		std::cout << x - str.length() ;
+		if(x == str.length()) std:: cout << ""; 
+		else std::cout << x - str.length() ;
 		std::cout << std::endl;
 	}
     return 0;
 }
-
