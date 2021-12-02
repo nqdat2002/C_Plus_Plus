7ef5876391754f46c7c28c7a88dc218d71177c07
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2021-12-02 21:24:36 +0700

    Update CPP0121.cpp

--- CPP0121.cpp
+++ CPP0121.cpp
@@ -1,30 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+void Solve(){
+	ll a, b; cin >> a >> b;
+	cout << (ll)a * b/__gcd(a, b) << " " << __gcd(a, b) << endl;
+}
 int main(){
-	int a,b,i;
-	int n;
-	cin >> n;
-	for(i=0;i<n;i++){
-		cin >> a >> b;
-		int m=a,n=b;
-		int c;
-		if(a==b){
-			cout << a << " " << b;
-		}
-		else
-		{
-			while(a!=b)
-			{
-				if(a>b) 
-				c=a-b;
-				else 
-				c=b-a;
-				a=b;
-				b=c;
-			}
-		cout << (long long int)m*n/c << " ";
-		cout << c << endl;	
-		}
+	int t; cin >> t;
+	while(t--){
+		Solve();
 	}
 	return 0;
 }
