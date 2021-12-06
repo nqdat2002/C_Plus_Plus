e54ebf907e5b1dd81c51741c25c4a549a207146a
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2021-12-06 09:56:02 +0700

    Update CPP0744.cpp

--- CPP0744.cpp
+++ CPP0744.cpp
@@ -1,25 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long arr[23];
+typedef long long ll;
+ll a[23];
+int n, r, g, b;
 void init(){
-	arr[0] = 1;
-	for(int i = 1; i <= 20; i ++){
-		arr[i] = arr[i - 1] * i;
-	}
+	a[0] = 1;
+	for(int i = 1; i <= 22; i ++)
+		a[i] = a[i - 1] * i;
 }
 void Solve(){
-	int N, G, R, B;
-	cin >> N >> G >> R >> B;
-	long long ans = 0;
-	for(int i = N; i >= R; i --){
-		for(int j = N - R; j >= G; j --){
-			int k = N - j - i;
-			if(k >= B){
-				ans += arr[N]/(arr[i] * arr[j] * arr[k]);
-			}
+	cin >> n >> g >> r >> b;
+	ll res = 0;
+	for(int i = n; i >= r; i --){
+		for(int j = n - r; j >= g; j --){
+			int k = n - j - i;
+			if(k >= b)
+				res += a[n]/(a[i] * a[j] * a[k]);	
 		}
 	}
-	cout << ans << endl;
+	cout << res << endl;
 }
 int main(){
 	int t; cin >> t;
@@ -29,4 +28,3 @@ int main(){
 	}
 	return 0;
 }
-
