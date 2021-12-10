959377185ad50db48b9df8af8fa16478966c13f5
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2021-12-10 10:12:04 +0700

    Update CPP0241.cpp

--- CPP0241.cpp
+++ CPP0241.cpp
@@ -2,29 +2,25 @@
 // Binary
 using namespace std; 
 int main() { 
-	int t; 
-	cin >> t; 
+	int t; cin >> t; 
 	while(t--) { 
-		int n; 
-		cin >> n; 
-		int arr[n]; 
-		for (int i = 0; i < n; i ++) 
-			cin >> arr[i]; 
+		int n; cin >> n; 
+		int a[n]; 
+		for (int i = 0; i < n; i ++) cin >> a[i]; 
 		int cnt = 0; 
-		int left = 0, right = n - 1; 
-		while(left <= right) { 
-			if (arr[left] == arr[right]) { 
-				left ++;
-			 	right --; 
+		int l = 0, r = n - 1; 
+		while(l <= r) { 
+			if (a[l] == a[r]) { 
+				l ++; r --; 
 			} 
-			else if (arr[left] < arr[right]) {
-				left ++; 
-				arr[left] += arr[left - 1]; 
-				cnt ++; 
+			else if (a[l] < a[r]) {
+				l ++; 
+				a[l] += a[l - 1]; 
+				cnt ++;
 			} 
 			else { 
-				right --; 
-				arr[right] += arr[right + 1]; 
+				r --; 
+				a[r] += a[r + 1]; 
 				cnt ++; 
 			} 
 		} 
